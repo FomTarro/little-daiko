c77ed26a4864dffd73a3480b57a5e5793f07220a
tom <twfarro@wpi.edu>
2021-07-18 16:53:49 -0400

    test cases for permissions

--- src/models/permissions.js
+++ src/models/permissions.js
@@ -2,13 +2,13 @@ function permissions(appConfig){
     const discordHelpers = appConfig.DISCORD_HELPERS;
     return {
         1: {
-            check: async(user, role) => { 
+            check: (user, role) => { 
                 return true;
             },
             description: `Any user`
         },
         2: {
-            check: async(user, role) => { 
+            check: (user, role) => { 
                 return discordHelpers.isBotOwner(user) 
                 || discordHelpers.isGuildOwner(user) 
                 || discordHelpers.isAdmin(user, role) 
@@ -16,14 +16,14 @@ function permissions(appConfig){
             description: `Bot Operator, Server Owner or Bot Owner`
         },
         3: {
-            check: async(user, role) => { 
+            check: (user, role) => { 
                 return discordHelpers.isBotOwner(user) 
                 || discordHelpers.isGuildOwner(user) 
             },
             description: `Server Owner or Bot Owner`
         },
         100: {
-            check: async(user, role) => { 
+            check: (user, role) => { 
                 return discordHelpers.isBotOwner(user) 
             },
             description: `Bot Owner`
---
+++ src/models/permissions.spec.js
@@ -0,0 +1,42 @@
+const { AppConfig } = require('../../app.config');
+
+describe("Permissions tests", () => {
+    const cases = [ 
+        ["Any User", false, false, false, true, false, false, false],
+        ["Bot Owner", true, false, false, true, true, true, true],
+        ["Server Owner", false, true, false, true, true, true, false],
+        ["Bot Operator", false, false, true, true, true, false, false],
+    ];
+    test.each(cases)("Permission: %p", async(
+        permissionChecked,
+        isBotOwner,
+        isGuildOwner,
+        isAdmin,
+        expectedAnyUser,
+        expectedHasRole,
+        expectedGuildOwner,
+        expectedBotOwner
+    ) => {
+        // set up mock dependencies
+        const dummyConfig = {
+            DISCORD_HELPERS: {
+                isBotOwner(){return isBotOwner},
+                isGuildOwner(){return isGuildOwner},
+                isAdmin(){return isAdmin},
+            },
+            PERMISSIONS: AppConfig.PERMISSIONS
+        }
+
+        // execute test
+        const dummyUser = {};
+        const permissions = AppConfig.PERMISSIONS(dummyConfig);
+        const anyUser = permissions[1];
+        expect(anyUser.check(dummyUser)).toBe(expectedAnyUser);
+        const hasRole = permissions[2];
+        expect(hasRole.check(dummyUser)).toBe(expectedHasRole);
+        const guildOwner = permissions[3];
+        expect(guildOwner.check(dummyUser)).toBe(expectedGuildOwner);
+        const botOwner = permissions[100];
+        expect(botOwner.check(dummyUser)).toBe(expectedBotOwner);
+    });
+});
